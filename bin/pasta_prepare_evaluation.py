4fdf55e5a6742a6c3344e705a777e5dd5fc4e723
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2020-08-31 18:30:25 +0200

    pypasta: introduce Linux.py
    Outsource things that can be reused by other users.
    
    No functional change.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- bin/pasta_prepare_evaluation.py
+++ bin/pasta_prepare_evaluation.py
@@ -23,6 +23,7 @@ from subprocess import call
 from tqdm import tqdm
 
 from pypasta import *
+from pypasta.Linux import load_characteristics_and_maintainers
 
 from pypasta.MailCharacteristics import MailCharacteristics, PatchType
 
